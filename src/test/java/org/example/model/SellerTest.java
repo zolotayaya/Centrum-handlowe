ef06214d87eda541111b8ebad91a9a4e16488674
Zlata Kenda <zlata@gmail.com>
2025-06-21 23:02:06 +0200

    test setup

---
+++ src/test/java/org/example/model/SellerTest.java
@@ -0,0 +1,14 @@
+package org.example.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SellerTest {
+
+    @Test
+    void test() {
+        assertTrue(true);
+    }
+
+}
